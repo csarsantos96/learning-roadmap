f09033dec3d05a13e22eb216030d459fddf67948
Cesar Santos <csantos1@sonda.com>
2026-04-20 09:12:06 -0500

    chore: initial commit - study planner setup

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'planner-cesar-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./index.html'));
+});
+
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SHOW_NOTIFICATION') {
+    self.registration.showNotification(e.data.title, {
+      body: e.data.body,
+      icon: './icons/icon-192.png',
+      badge: './icons/icon-192.png',
+      tag: e.data.tag || 'planner',
+      requireInteraction: false,
+      vibrate: [200, 100, 200]
+    });
+  }
+});
